dc44d92be017cf1ec279fe871f4925fd5bfaa49c
RAVINIVASAN <nivasravi412@gmail.com>
2026-02-27 11:58:13 +0530

    OPacity 5

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <motion.section
       className="hero text-center text-light"
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{ opacity: 5 }}
       transition={{ duration: 1 }}
     >
       <div className="container">
