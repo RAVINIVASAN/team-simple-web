47a8fe4ed61bfe9c9ca4729c46bd487debacac8d
RAVINIVASAN <nivasravi412@gmail.com>
2026-02-27 10:29:55 +0530

    feat: add Navbar, update App.js, and implement Theme support

---
+++ frontend/src/app.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
